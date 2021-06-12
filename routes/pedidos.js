f1bb0a96cbc04c7a89bc2d0e64d77698ae5f054a
Erik Meisel <erikmeisel@gmail.com>
2021-06-12 03:00:30 -0300

    borrar y marcar pedido como entregado

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -28,15 +28,14 @@ let objectId = mongodb.ObjectId;
 //  /pedidos
 router.get('/', async function(req, res, next) {
   const clientmongo = await connection.getConnection();
-  const pedidos = await clientmongo.db('PaulCocina_DB')
-                  .collection('pedidos')
-                  .find()
-                  .toArray();
-  if(pedidos){    
-    res.json(pedidos);
-  } else {
-    res.status(404).send('No hay pedidos');
-  }
+  clientmongo.db('PaulCocina_DB')
+      .collection('pedidos')
+      .find()
+      .toArray().then(pedidos=>{
+        res.status(200).send(pedidos);
+      }).catch(error=>{
+        res.status(400).send(error);
+      })
 });
 
 router.get('/:id', async (req,res)=>{
@@ -78,36 +77,36 @@ router.post('/', async (req, res)=>{
     }
 });
 
-// /recetas/id
 router.put('/:id', async (req, res)=>{
-  const schema = joi.object({
-    titulo: joi.string().min(5),
-    descripcion: joi.string().min(15),
-    instrucciones:joi.string().min(20),
-    foto:joi.string().min(5), //tiene que ser una url
-    categoria:joi.string().min(5),
-    ingredientes:joi.array().items(joi.string().min(2)) // tiene que ser parte de los ingredientes
-  });
-  const result = schema.validate(req.body);
-  if(result.error){
-      res.status(400).send(result.error.details[0].message);
-  } else{
-      let receta = req.body;
-      receta._id = req.params.id;
-      dataReceta.updateReceta(receta);
-      res.json(receta);
+  const clientmongo = await connection.getConnection();
+  const query = {_id: new objectId(req.params.id)};
+  const newvalues = { $set:{
+          estado: req.body.estado
+      }
   }
-});
+  const result = await clientmongo.db('PaulCocina_DB')
+        .collection('pedidos')
+        .updateOne(query, newvalues)
+        .then(data => {
+          res.status(200).send(data)
+        }).catch(error => {
+          console.log(error)
+          res.status(400).send(error)
+        })
 
-// /recetas/id
+})
+
 router.delete('/:id', async (req, res)=>{
-  const receta = await dataReceta.getReceta(req.params.id)
-  if(!receta){
-      res.status(404).send('Receta no encontrada');
-  } else {
-      dataReceta.deleteReceta(req.params.id);
-      res.status(200).send('Receta eliminada');
-  }
+  const clientmongo = await connection.getConnection();
+  clientmongo.db('PaulCocina_DB')
+      .collection('pedidos')
+      .deleteOne({_id: new objectId(req.params.id)})
+      .then(data => {
+        res.status(200).send(data)
+      }).catch(error => {
+        console.log(error)
+        res.status(400).send(error)
+      })
 });
 
 export {router};
