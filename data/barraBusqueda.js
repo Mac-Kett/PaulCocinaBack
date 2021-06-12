45eb80127e9333944e07dff30b31dec382764036
Juan Manuel <juanma3405@hotmail.com>
2021-06-12 19:01:20 -0300

    agregue a la busqueda por nombre de receta y sin distincion de mayusculas y minusculas

--- data/barraBusqueda.js
+++ data/barraBusqueda.js
@@ -1,14 +1,16 @@
 import connection from './connection.js';
-//import mongodb from 'mongodb';
-//let objectId = mongodb.ObjectId;
 
-//buscamos un ingrediente
 
 async function getElementoBuscado(elemento){
    const clientmongo = await connection.getConnection();
     const recetas = await clientmongo.db('PaulCocina_DB')
                     .collection('recipes')
-                    .find({"ingredientes": elemento})
+                    .find ({
+                        $or : [
+                        {"titulo":{$regex: elemento, $options:"i"}},
+                        {"ingredientes":{$regex: elemento, $options:"i"}}, 
+                        ]
+                    })
                     .toArray();
     return recetas;
 }
